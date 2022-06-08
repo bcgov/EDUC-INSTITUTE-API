97af710427414f579b1f7ce6306c5e9c8d28e89b
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-06-07 18:35:53 -0700

    Test fix.

--- api/src/main/java/ca/bc/gov/educ/api/institute/InstituteApiResourceApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/institute/InstituteApiResourceApplication.java
@@ -3,15 +3,16 @@ package ca.bc.gov.educ.api.institute;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * The type School api resource application.
@@ -39,7 +40,7 @@ public class InstituteApiResourceApplication {
    */
   @Configuration
   static
-  class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+  class WebSecurityConfiguration {
 
     /**
      * Instantiates a new Web security configuration.
@@ -50,23 +51,20 @@ public class InstituteApiResourceApplication {
       SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 
-    /**
-     * Configure paths to be excluded from security.
-     *
-     * @param web the web
-     */
-    @Override
-    public void configure(WebSecurity web) {
-      web.ignoring().antMatchers("/v3/api-docs/**",
-          "/actuator/health", "/actuator/prometheus","/actuator/**",
-          "/swagger-ui/**");
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+      return (web) -> web.ignoring().antMatchers("/v3/api-docs/**",
+        "/actuator/health", "/actuator/prometheus","/actuator/**",
+        "/swagger-ui/**");
     }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
       http
-          .authorizeRequests()
-          .anyRequest().authenticated().and()
-          .oauth2ResourceServer().jwt();
+        .authorizeRequests()
+        .anyRequest().authenticated().and()
+        .oauth2ResourceServer().jwt();
+      return http.build();
     }
   }
 
