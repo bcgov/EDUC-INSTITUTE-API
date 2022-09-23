ac4badfc5e1f211ced685dba09d229f6c15ee0dd
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-09-23 13:56:46 -0700

    Added early learning/childcare lead.

---
+++ api/src/main/resources/db/migration/V1.0.3__INSTITUTE_API.sql
@@ -0,0 +1,8 @@
+INSERT INTO DISTRICT_CONTACT_TYPE_CODE (DISTRICT_CONTACT_TYPE_CODE, LABEL, DESCRIPTION, LEGACY_CODE,
+                                        DISPLAY_ORDER, EFFECTIVE_DATE, EXPIRY_DATE, CREATE_USER,
+                                        CREATE_DATE, UPDATE_USER, UPDATE_DATE)
+VALUES ('EAR_CHILD', 'Early Learning/Childcare Lead', 'Early Learning and Childcare Lead', null, 10,
+        to_date('2020-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'),
+        to_date('2099-12-31 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'IDIR/MVILLENE',
+        to_date('2019-12-20 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'IDIR/MVILLENE',
+        to_date('2019-12-20 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
