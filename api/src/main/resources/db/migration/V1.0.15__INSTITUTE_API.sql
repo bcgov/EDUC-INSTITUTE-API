c8b6edd66365ebccbd64b87b3b4de3e5f656fcef
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-11-22 17:26:19 -0800

    Change to youth custody label.

---
+++ api/src/main/resources/db/migration/V1.0.15__INSTITUTE_API.sql
@@ -0,0 +1,6 @@
+UPDATE FACILITY_TYPE_CODE
+SET
+    LABEL = 'Youth Custody/Residential',
+    DESCRIPTION = 'Youth custody or residential attendance school'
+WHERE
+        FACILITY_TYPE_CODE = 'YOUTH';
