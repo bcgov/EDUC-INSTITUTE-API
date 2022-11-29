97ab70e95ab3cd5bdc7322393f80513d1887c3a5
Tim Osse <tim.osse@cgi.com>
2022-11-28 16:05:06 -0800

    Increment Authroity Number when creating a new Authority - Refactored - Code smell fix

--- api/src/main/java/ca/bc/gov/educ/api/institute/repository/v1/IndependentAuthorityRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/institute/repository/v1/IndependentAuthorityRepository.java
@@ -4,7 +4,7 @@ import ca.bc.gov.educ.api.institute.model.v1.IndependentAuthorityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+
 import java.util.UUID;
 
 @Repository
