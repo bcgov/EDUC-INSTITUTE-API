85898f5c3232bc8147cf413dcbf2f28721402104
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-12-13 11:03:04 -0800

    Testing for pagination.

--- api/src/main/java/ca/bc/gov/educ/api/institute/config/AsyncConfiguration.java
+++ api/src/main/java/ca/bc/gov/educ/api/institute/config/AsyncConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@EnableAsync
 @Profile("!test")
 public class AsyncConfiguration {
   /**
