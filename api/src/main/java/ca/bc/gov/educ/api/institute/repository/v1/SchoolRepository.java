676e40280ece2be31c44f303eef9b53897518d50
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2023-02-13 15:46:32 -0800

    Update SchoolRepository.java

--- api/src/main/java/ca/bc/gov/educ/api/institute/repository/v1/SchoolRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/institute/repository/v1/SchoolRepository.java
@@ -12,30 +12,36 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public interface SchoolRepository extends JpaRepository<SchoolEntity, UUID>, JpaSpecificationExecutor<SchoolEntity> {
   @Transactional
-  @Query(value = "SELECT MAX(S.SCHOOL_NUMBER)\n" +
-          "FROM SCHOOL S, DISTRICT D\n" +
-          "WHERE S.DISTRICT_ID = D.DISTRICT_ID\n" +
-          "AND D.DISTRICT_NUMBER = :districtNumber\n" +
-          "AND S.SCHOOL_NUMBER LIKE :pattern \n" +
-          "AND S.INDEPENDENT_AUTHORITY_ID IS NOT DISTINCT FROM :authorityId", nativeQuery = true)
+  @Query(value = """
+          SELECT MAX(S.SCHOOL_NUMBER)
+          FROM SCHOOL S, DISTRICT D
+          WHERE S.DISTRICT_ID = D.DISTRICT_ID
+          AND D.DISTRICT_NUMBER = :districtNumber
+          AND S.SCHOOL_NUMBER LIKE :pattern
+          AND S.INDEPENDENT_AUTHORITY_ID IS NOT DISTINCT FROM :authorityId"""
+          , nativeQuery = true)
   String findLastSchoolNumberWithPattern(String districtNumber, UUID authorityId, String pattern);
 
   @Transactional
-  @Query(value = "SELECT MAX(S.SCHOOL_NUMBER)\n" +
-          "FROM SCHOOL S, DISTRICT D\n" +
-          "WHERE S.DISTRICT_ID = D.DISTRICT_ID\n" +
-          "AND D.DISTRICT_NUMBER = :districtNumber\n" +
-          "AND S.INDEPENDENT_AUTHORITY_ID IS NOT DISTINCT FROM :authorityId", nativeQuery = true)
+  @Query(value = """
+          SELECT MAX(S.SCHOOL_NUMBER)
+          FROM SCHOOL S, DISTRICT D
+          WHERE S.DISTRICT_ID = D.DISTRICT_ID
+          AND D.DISTRICT_NUMBER = :districtNumber
+          AND S.INDEPENDENT_AUTHORITY_ID IS NOT DISTINCT FROM :authorityId"""
+          , nativeQuery = true)
   String findLastSchoolNumber(String districtNumber, UUID authorityId);
 
   @Transactional
-  @Query(value = "SELECT MIN(S.ID) AS MISSING_NUM\n" +
-          "FROM generate_series(:lowerRange, :upperRange) S(ID)\n" +
-          "WHERE NOT EXISTS\n" +
-          "(SELECT 1 FROM SCHOOL SCH, DISTRICT DIS\n" +
-          "WHERE CAST(SCH.SCHOOL_NUMBER as INTEGER) = S.ID\n" +
-          "AND SCH.DISTRICT_ID = DIS.DISTRICT_ID\n" +
-          "AND DIS.DISTRICT_NUMBER = :districtNumber\n" +
-          "AND SCH.INDEPENDENT_AUTHORITY_ID IS NOT DISTINCT FROM :authorityId)", nativeQuery = true)
+  @Query(value = """
+          SELECT MIN(S.ID) AS MISSING_NUM
+          FROM generate_series(:lowerRange, :upperRange) S(ID)
+          WHERE NOT EXISTS
+          (SELECT 1 FROM SCHOOL SCH, DISTRICT DIS
+          WHERE CAST(SCH.SCHOOL_NUMBER as INTEGER) = S.ID
+          AND SCH.DISTRICT_ID = DIS.DISTRICT_ID
+          AND DIS.DISTRICT_NUMBER = :districtNumber
+          AND SCH.INDEPENDENT_AUTHORITY_ID IS NOT DISTINCT FROM :authorityId)"""
+          , nativeQuery = true)
   Integer findFirstAvailableSchoolNumber(String districtNumber, UUID authorityId, Integer lowerRange, Integer upperRange);
 }
