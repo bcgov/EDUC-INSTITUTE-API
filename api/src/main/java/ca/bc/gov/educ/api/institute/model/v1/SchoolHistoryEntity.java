5ab56826797f81ead5142911daa9e28f694bee73
Trevor Richards <trev@trevdev.ca>
2023-03-31 12:46:09 -0700

    chore: add some whitespace

--- api/src/main/java/ca/bc/gov/educ/api/institute/model/v1/SchoolHistoryEntity.java
+++ api/src/main/java/ca/bc/gov/educ/api/institute/model/v1/SchoolHistoryEntity.java
@@ -24,58 +24,76 @@ public class SchoolHistoryEntity {
     @org.hibernate.annotations.Parameter(name = "uuid_gen_strategy_class", value = "org.hibernate.id.uuid.CustomVersionOneStrategy")})
   @Column(name = "school_history_id", unique = true, updatable = false, columnDefinition = "BINARY(16)")
   private UUID schoolHistoryId;
+
   @Basic
   @Column(name = "school_id", columnDefinition = "BINARY(16)")
   private UUID schoolId;
+
   @Basic
   @Column(name = "district_id", columnDefinition = "BINARY(16)")
   private UUID districtID;
+
   @Basic
   @Column(name = "independent_authority_id", columnDefinition = "BINARY(16)")
   private UUID independentAuthorityId;
+
   @Basic
   @Column(name = "school_number")
   private String schoolNumber;
+
   @Basic
   @Column(name = "fax_number")
   private String faxNumber;
+
   @Basic
   @Column(name = "phone_number")
   private String phoneNumber;
+
   @Basic
   @Column(name = "email")
   private String email;
+
   @Basic
   @Column(name = "website")
   private String website;
+
   @Basic
   @Column(name = "display_name")
   private String displayName;
+
   @Basic
   @Column(name = "school_organization_code")
   @UpperCase
   private String schoolOrganizationCode;
+
   @Basic
   @Column(name = "school_category_code")
   @UpperCase
   private String schoolCategoryCode;
+
   @Basic
   @Column(name = "facility_type_code")
   @UpperCase
   private String facilityTypeCode;
+
   @Basic
   @Column(name = "opened_date")
   private LocalDateTime openedDate;
+
   @Basic
   @Column(name = "closed_date")
   private LocalDateTime closedDate;
+
   @Column(name = "CREATE_USER", updatable = false)
   private String createUser;
+
   @PastOrPresent
   @Column(name = "CREATE_DATE", updatable = false)
   private LocalDateTime createDate;
+
   @Column(name = "update_user")
   private String updateUser;
+
   @PastOrPresent
   @Column(name = "update_date")
   private LocalDateTime updateDate;
