d00e5a9abd87952f6b564c342c80f2d98b2e3e2d
SoLetsDev <74216496+SoLetsDev@users.noreply.github.com>
2023-04-12 14:24:22 -0700

    Adding in flyway script to add in schoolMoveHistory table. Minor change to primary key for school Move history.

--- api/src/main/java/ca/bc/gov/educ/api/institute/model/v1/SchoolMoveHistoryEntity.java
+++ api/src/main/java/ca/bc/gov/educ/api/institute/model/v1/SchoolMoveHistoryEntity.java
@@ -30,7 +30,7 @@ public class SchoolMoveHistoryEntity {
   @GeneratedValue(generator = "UUID")
   @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator", parameters = {
     @org.hibernate.annotations.Parameter(name = "uuid_gen_strategy_class", value = "org.hibernate.id.uuid.CustomVersionOneStrategy")})
-  @Column(name = "school_move_id", unique = true, updatable = false, columnDefinition = "BINARY(16)")
+  @Column(name = "school_move_history_id", unique = true, updatable = false, columnDefinition = "BINARY(16)")
   @ComparableField
   private UUID schoolMoveHistoryId;
 
